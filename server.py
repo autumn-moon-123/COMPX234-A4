66e2d9f4a1c923f37a708b075bc0d487d0748a47
autumn-moon-123 <2387105387@qq.com>
2025-06-19 02:42:23 +0800

    5 save

--- server.py
+++ server.py
@@ -4,9 +4,18 @@ import os
 import random
 import base64
 import sys  
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(('127.0.0.1', 12345))
+
+class UDPServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.bind((host, port))
+        self.file_dir = os.getcwd()
+
+        server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        server.bind(('127.0.0.1', 12345))
 
 filename = server.recvfrom(1024)[0].decode()
 with open(filename, 'rb') as f:
-    server.sendto(f.read(), ('127.0.0.1', 12346)) 
+        server.sendto(f.read(), ('127.0.0.1', 12346)) 
